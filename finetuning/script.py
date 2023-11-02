760b1566bd392947901e31cd371764dcdc1a6276
trinhlehoangminh2000 <trinhlehoangminh2000@gmail.com>
2023-11-02 09:54:33 +0100

    Restructure repo

--- finetuning/script.py
+++ finetuning/script.py
@@ -1,87 +1,76 @@
-# %%
-from influencer_profiler import InfluencerProfiler, save_model
-from dataloader import create_data_loader
-# from dataloader_v2 import create_data_loader
-from train_loop import train_loop
-from transformers import AutoTokenizer, get_linear_schedule_with_warmup
 
 import pandas as pd
-from torch import optim
-import torch.nn as nn
-import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import TrainingArguments, Trainer
+from compute_metrics import compute_metrics
+from TweetDataset import TweetDataset
+from transformers import EarlyStoppingCallback
 
-# %%
-MODELS = ['charlieoneill/distilbert-base-uncased-finetuned-tweet_eval-offensive', 
-         'pig4431/TweetEval_roBERTa_5E', 
-         'tner/twitter-roberta-base-dec2021-tweetner7-random',
-         'cardiffnlp/tweet-topic-21-multi',
-         'cardiffnlp/twitter-roberta-base-2021-124m'
-         ]
+MODELS = [  'tner/roberta-large-tweetner7-all',
+            'tner/bertweet-large-tweetner7-all',
+            'cardiffnlp/twitter-roberta-large-2022-154m',
+            'roberta-large',
+            'google/electra-large-discriminator',
+]
 
-# too large 'tner/bertweet-large-tweetner7-all',
+BATCH_SIZE = 4
+EPOCHS = 50
+TOKENS = 512
 
-EPOCHS = 4
+train_df = pd.read_csv('../data/finetune_train_val_test/train.csv')
+validate_df = pd.read_csv('../data/finetune_train_val_test/validate.csv')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def translate_class_column(df):
-    class_map = {
-        "no influencer": 0,
-        "nano": 1,
-        "micro": 2,
-        "macro": 3,
-        "mega": 4
-    }
-    
-    if "class" not in df.columns:
-        print("Error: DataFrame does not contain a column called 'class'")
-        return
-    
-    df["class"] = df["class"].apply(lambda x: class_map.get(x, x))
-    
-    return df
+for model_name in MODELS:
+    print(f"Training {model_name}:")
 
-df_train = translate_class_column(pd.read_csv('../data/finetune_train_val_test/train.csv'))
-df_val = translate_class_column(pd.read_csv('../data/finetune_train_val_test/validate.csv'))
-df_test = translate_class_column(pd.read_csv('../data/finetune_train_val_test/test.csv'))
-
-# %%
-for model_name in MODELS:    
-    print(f'Training {model_name}')
-    # Retrieve the tokenizer for the model
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name,
+                                                        num_labels=5,
+                                                        problem_type="multi_label_classification",
+                                                        ignore_mismatched_sizes=True)
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        model.resize_token_embeddings(len(tokenizer))
 
-    # Create data loader
-    train_data_loader = create_data_loader(df_train, tokenizer, 512, 4)
-    val_data_loader = create_data_loader(df_val, tokenizer, 512, 4)
-    test_data_loader = create_data_loader(df_test, tokenizer, 512, 4)
+    train_dataset = TweetDataset(tokenizer, TOKENS, train_df, strategy='concat')
+    val_dataset = TweetDataset(tokenizer, TOKENS, validate_df, strategy='concat')
 
-    # Create model
-    model = InfluencerProfiler(model= model_name, n_classes=5).to(device)
-    model.requires_grad_embeddings(True)
+    train_steps_per_epoch = len(train_dataset) // BATCH_SIZE
 
-    # Set training parameters
-    optimizer = optim.AdamW(model.parameters(), lr=2e-5)
-    total_steps = len(train_data_loader) * EPOCHS
-    scheduler = get_linear_schedule_with_warmup(
-                    optimizer,
-                    num_warmup_steps=0,
-                    num_training_steps=total_steps
-                )
+    training_arguments = TrainingArguments(
+        output_dir=f'./results/{model_name}',
+        evaluation_strategy = "epoch",
+        save_strategy = "epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
+        num_train_epochs=EPOCHS,
+        weight_decay=0.01,
+        load_best_model_at_end=True,
+        metric_for_best_model= "eval_f1",
+        save_total_limit = 3,
+        warmup_steps=100,
+        logging_dir='./logs',
+        logging_steps = train_steps_per_epoch,
+        disable_tqdm=True
+    )
 
-    # Set loss function
-    loss_fn = nn.CrossEntropyLoss().to(device)
+    trainer = Trainer(
+        model,
+        training_arguments,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        tokenizer= tokenizer,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
+    )
 
-    # Train model
-    model = train_loop(EPOCHS, train_data_loader, val_data_loader, model, loss_fn, optimizer, device, scheduler)
+    trainer.train()
 
-    # Export model
-    save_as = model_name.split('/', 1)[1]
-    save_model(model, f'{save_as}.pth')
-
-    # Free GPU mem from model
-    del(model)
-    torch.cuda.empty_cache()
+    trainer.save_model(model_name)
     print()
+    del(model)
+
 
 
